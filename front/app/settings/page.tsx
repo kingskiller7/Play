b5211df0eff9ac8496bc9b19cae0f9a9820ed12f
kingskiller7 <abdulmustafa124@gmail.com>
2024-12-09 17:20:51 +0530

    Resolve Error Typo.

--- front/app/settings/page.tsx
+++ front/app/settings/page.tsx
@@ -6,7 +6,7 @@ import { Layout } from '@/components/layout'
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
-import { Label } from "@/components/components/ui/label"
+import { Label } from "@/components/ui/label"
 import api from '@/lib/api'
 
 export default function Settings() {
