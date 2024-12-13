794cbe0de4e5a2862382b09a56b2b3b63229f277
kingskiller7 <abdulmustafa124@gmail.com>
2024-12-13 17:32:03 +0530

    Change Font to Comfortaa.

--- front/app/layout.tsx
+++ front/app/layout.tsx
@@ -1,17 +1,13 @@
 import { AuthProvider } from '@/contexts/AuthContext'
 import './globals.css'
 import type { Metadata } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
+import { Comfortaa } from 'next/font/google'
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const comfortaa = Comfortaa({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-comfortaa',
+})
 
 export const metadata: Metadata = {
   title: 'CyberAuth',
@@ -25,7 +21,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className={comfortaa.variable}>
         <AuthProvider>
           {children}
         </AuthProvider>
