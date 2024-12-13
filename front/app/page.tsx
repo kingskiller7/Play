3247baa74c6740fd4e20e87bfbf4a2f852a0dc20
kingskiller7 <abdulmustafa124@gmail.com>
2024-12-13 18:05:08 +0530

    Modify app/page.tsx.

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
       <main className="flex-1">
         <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48">
           <div className="container px-4 md:px-6">
-            <div className="flex flex-col items-center space-y-4 text-center">
+            <div className="flex flex-col items-center text-center">
               <div className="space-y-2">
                 <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
                   Next-gen Authentication System
