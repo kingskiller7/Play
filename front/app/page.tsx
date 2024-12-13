dcf20de00bc1cdffcdcae6ef4d1ef05ad6ca4fc6
kingskiller7 <abdulmustafa124@gmail.com>
2024-12-13 18:34:03 +0530

    Modify app/page.tsx.

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -53,7 +53,7 @@ export default function Home() {
         </section>
       </main> */}
       <main className="flex-1">
-        <div className="container w-full py-12 md:py-24 lg:py-32 xl:py-48">
+        <div className="w-full py-12 md:py-24 lg:py-32 xl:py-48">
           <div className="flex flex-col items-center space-y-4 text-center">
             <div className="space-y-2">
               <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
